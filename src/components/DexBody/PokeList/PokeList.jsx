015f9186a2770af650f90163bab72c07a733dabf
edgz98 <edgz98@hotmail.com>
2024-10-27 16:30:58 +0900

    Add SortBy: Dropdown options and Functionality

--- src/components/DexBody/PokeList/PokeList.jsx
+++ src/components/DexBody/PokeList/PokeList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PokeList.css";
 import ErrorScreen from "../../Common/ErrorScreen/ErrorScreen";
 import LoadingScreen from "../../Common/LoadingScreen/LoadingScreen";
@@ -10,8 +10,26 @@ import Pagination from "./Pagination/Pagination.jsx";
 function PokeList() {
   const { pokemonList, loading, error } = usePokemonData(493);
   const itemsPerPage = 16;
+
+  const [sortBy, setSortBy] = useState("Default");
+
+  const sortedPokemonList = [...pokemonList].sort((a, b) => {
+    switch (sortBy) {
+      case "A-Z":
+        return a.name.localeCompare(b.name);
+      case "Z-A":
+        return b.name.localeCompare(a.name);
+      case "Lowest #":
+        return a.id - b.id;
+      case "Highest #":
+        return b.id - a.id;
+      default:
+        return 0;
+    }
+  });
+
   const { currentItems, currentPage, totalPages, handlePageChange } =
-    usePagination(pokemonList, itemsPerPage);
+    usePagination(sortedPokemonList, itemsPerPage);
 
   if (loading) {
     return <LoadingScreen />;
@@ -23,6 +41,23 @@ function PokeList() {
 
   return (
     <div className="poke-list-wrapper">
+      <div className="sort-container">
+        <label htmlFor="sort" className="sort-label">
+          Sort by:
+        </label>
+        <select
+          className="sort-selection"
+          id="sort"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="A-Z">A-Z</option>
+          <option value="Z-A">Z-A</option>
+          <option value="Lowest #">Lowest #</option>
+          <option value="Highest #">Highest #</option>
+        </select>
+      </div>
+
       <div className="list-main-container">
         {currentItems.map((pokemon) => {
           const pokeItemProps = {
